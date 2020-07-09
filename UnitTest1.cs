20b665f01de23cdf56f98291a7caac1ec0cdb20a
Vijayakumar3300 <67868565+Vijayakumar3300@users.noreply.github.com>
2020-07-09 08:34:40 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using Helper;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_PhysicalProduct()
+        {
+            string msg="";
+            IPaymentInterface paymentObject = PaymentObjectService.GetPaymentForObjectDetails((int)PaymentItemValues.PhysicalProduct);
+            bool result = paymentObject.ProcessPayment(out msg);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void TestMethod_PhysicalProduct()
+        {
+            string msg = "";
+            IPaymentInterface paymentObject = PaymentObjectService.GetPaymentForObjectDetails((int)PaymentItemValues.PhysicalProduct);
+            bool result = paymentObject.ProcessPayment(out msg);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void TestMethod_PhysicalProduct()
+        {
+            string msg = "";
+            IPaymentInterface paymentObject = PaymentObjectService.GetPaymentForObjectDetails((int)PaymentItemValues.PhysicalProduct);
+            bool result = paymentObject.ProcessPayment(out msg);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void TestMethod_PhysicalProduct()
+        {
+            string msg = "";
+            IPaymentInterface paymentObject = PaymentObjectService.GetPaymentForObjectDetails((int)PaymentItemValues.PhysicalProduct);
+            bool result = paymentObject.ProcessPayment(out msg);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void TestMethod_PhysicalProduct()
+        {
+            string msg = "";
+            IPaymentInterface paymentObject = PaymentObjectService.GetPaymentForObjectDetails((int)PaymentItemValues.PhysicalProduct);
+            bool result = paymentObject.ProcessPayment(out msg);
+            Assert.AreEqual(result, true);
+        }
+
+    }
+}
